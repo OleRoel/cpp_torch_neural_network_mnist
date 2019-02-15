c693b6aed9c4e6930bd4eadf0d713dbf7db2767d
Ole Roel <info@oleroel.de>
2019-02-15 18:37:12 +0100

    Code cleanup

--- mnist.cpp
+++ mnist.cpp
@@ -34,7 +34,8 @@ void run_train(torch::nn::Sequential& model, Dataset& ds, torch::Device& device)
     auto train_loader =
         torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
         std::move(ds), kBatchSize);
-    torch::Tensor fake_labels = torch::nn::init::constant_(torch::empty(10, device), 0.01f);
+    torch::Tensor fake_labels = torch::nn::init::constant_(
+        torch::empty(10, device), 0.01f);
 
     torch::optim::SGD model_optimizer(model->parameters(),
                       torch::optim::SGDOptions(kLearningRate));
@@ -50,6 +51,7 @@ void run_train(torch::nn::Sequential& model, Dataset& ds, torch::Device& device)
                 torch::Tensor output = model->forward(real_images[i].reshape(kInputSize));
                 
                 fake_labels[targets[i]] = 0.99;
+
         		torch::Tensor loss = torch::mse_loss(output, fake_labels, Reduction::None);
                 model_optimizer.zero_grad();
                 loss.backward();
@@ -76,7 +78,8 @@ void run_test(torch::nn::Sequential& model, Dataset& ds, torch::Device& device) 
             torch::Tensor output = model->forward(real_images[i].reshape(784));
             int64_t numel = output.numel();
             int64_t expected = targets[i].data<int64_t>()[0];
-            int64_t label = std::distance(output.data<float>(), std::max_element(output.data<float>(), output.data<float>()+numel));
+            int64_t label = std::distance(output.data<float>(),
+                            std::max_element(output.data<float>(), output.data<float>()+numel));
             // std::cout << "numel: " << numel << std::endl;
             // std::cout << "expected: " << expected << std::endl;
             // std::cout << "label: " << label << std::endl;
@@ -100,52 +103,49 @@ void run_test(torch::nn::Sequential& model, Dataset& ds, torch::Device& device) 
 }
 
 int main() {
-  torch::manual_seed(1);
+    torch::manual_seed(1);
 
-  // Create the device we pass around based on whether CUDA is available.
-  torch::Device device(torch::kCPU);
-  if (torch::cuda::is_available()) {
-    std::cout << "CUDA is available! Training on GPU." << std::endl;
-    device = torch::Device(torch::kCUDA);
-  }
+    // Create the device we pass around based on whether CUDA is available.
+    torch::Device device(torch::kCPU);
+    if (torch::cuda::is_available()) {
+        std::cout << "CUDA is available! Training on GPU." << std::endl;
+        device = torch::Device(torch::kCUDA);
+    }
 
-  torch::nn::Sequential model(
-      torch::nn::Linear(torch::nn::LinearOptions(kInputSize, kHiddenSize).with_bias(false)),
-      torch::nn::Functional(torch::sigmoid),
-      torch::nn::Linear(torch::nn::LinearOptions(kHiddenSize, kNumClasses).with_bias(false)),
-      torch::nn::Functional(torch::sigmoid)
-      );
+    torch::nn::Sequential model(
+        torch::nn::Linear(torch::nn::LinearOptions(kInputSize, kHiddenSize).with_bias(false)),
+        torch::nn::Functional(torch::sigmoid),
+        torch::nn::Linear(torch::nn::LinearOptions(kHiddenSize, kNumClasses).with_bias(false)),
+        torch::nn::Functional(torch::sigmoid)
+    );
 
-  for (auto&& layer : *model) {
-    std::shared_ptr<torch::nn::Module> module_ptr = layer.ptr();
-    module_ptr->apply([](torch::nn::Module& module) {
-       torch::NoGradGuard no_grad;
-       if (auto* linear = module.as<torch::nn::Linear>()) {
-            std::cout << "i.sizes():\n" << linear->weight.sizes() << std::endl;
-            float N = linear->weight.sizes()[1];
-            float sigma{std::pow(N, -0.5F)};
-            std::cout << "sigma: " << sigma << std::endl;
-            linear->weight.normal_(0.0, sigma);
-       }
-    });
-  }
+    for (auto&& layer : *model) {
+        std::shared_ptr<torch::nn::Module> module_ptr = layer.ptr();
+        module_ptr->apply([](torch::nn::Module& module) {
+            torch::NoGradGuard no_grad;
+            if (auto* linear = module.as<torch::nn::Linear>()) {
+                float N = linear->weight.sizes()[1];
+                float sigma{std::pow(N, -0.5F)};
+                linear->weight.normal_(0.0, sigma);
+            }
+        });
+    }
 
-  model->to(device);
-#if 1
+    model->to(device);
+
     auto start_time = std::chrono::high_resolution_clock::now();
-#if 1
     {
         auto ds_mnist = torch::data::datasets::MNIST("./mnist")
                 .map(NORMALIZE(0.5, 0.5))
                 .map(STACK());
-        auto& t1 = std::chrono::high_resolution_clock::now();
+        auto t1 = std::chrono::high_resolution_clock::now();
         run_train(model, ds_mnist, device);
         auto t2 = std::chrono::high_resolution_clock::now();
         std::cout << "training took "
                 << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count()
                 << " milliseconds" << std::endl;
     }
-#endif
+
     {
         auto ds_mnist = torch::data::datasets::MNIST("./mnist")
                 .map(NORMALIZE(0.5, 0.5))
@@ -163,5 +163,4 @@ int main() {
     std::cout << "total run-time "
             << std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count()
             << " milliseconds" << std::endl;    
-#endif
 }
